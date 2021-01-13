f37238f26a42947141acf7eb4086a6703f45b467
callkend <77285430+callkend@users.noreply.github.com>
2021-01-13 08:34:05 -0700

    Operators

--- Operators.vb
+++ Operators.vb
@@ -2,6 +2,14 @@
 
     Sub Main()
 
+        Console.WriteLine(2 + 2)
+
+        Console.WriteLine(9 / 3)    'Regualar Division
+        Console.WriteLine(9 \ 3)    'Interger
+        Console.WriteLine(9 Mod 3)  'Divison that returns only the remainder
+
+        Console.ReadLine()
+
     End Sub
 
 End Module
