15e203746aa120224384b70da5018bfe236a4353
callkend <77285430+callkend@users.noreply.github.com>
2021-01-13 08:39:04 -0700

    exponent

--- Operators.vb
+++ Operators.vb
@@ -8,6 +8,8 @@
         Console.WriteLine(9 \ 3)    'Interger
         Console.WriteLine(9 Mod 3)  'Divison that returns only the remainder
 
+        Console.WriteLine(5 ^ 5)
+
         Console.ReadLine()
 
     End Sub
